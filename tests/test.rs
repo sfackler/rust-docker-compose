8fec05f1f45245394bcfda613a2e98f16ea667a1
Steven Fackler <sfackler@palantir.com>
2016-05-31 10:13:05 -0700

    Make test a bit more robust

--- tests/test.rs
+++ tests/test.rs
@@ -2,15 +2,21 @@ extern crate env_logger;
 extern crate docker_compose;
 
 use docker_compose::DockerComposition;
+use std::io::Read;
 use std::net::TcpStream;
 
 fn main() {
     env_logger::init().unwrap();
 
-    let _docker = DockerComposition::builder()
-                      .check(check_port)
-                      .build(concat!(env!("CARGO_MANIFEST_DIR"), "/tests/docker-compose.yml"))
-                      .unwrap();
+    let docker = DockerComposition::builder()
+                     .check(check_port)
+                     .build(concat!(env!("CARGO_MANIFEST_DIR"), "/tests/docker-compose.yml"))
+                     .unwrap();
+    let port = docker.port("test", 1234).unwrap();
+    let mut stream = TcpStream::connect(("localhost", port)).unwrap();
+    let mut out = String::new();
+    stream.read_to_string(&mut out).unwrap();
+    assert_eq!(out, "hello");
 }
 
 fn check_port(composition: &DockerComposition) -> bool {
